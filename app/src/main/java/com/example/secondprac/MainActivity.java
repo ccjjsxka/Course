4b2ac740221ff2bb48dcfc733e17ffd28560865b
liang <1912716826@qq.com>
2020-09-15 22:16:16 +0800

    first

---
+++ app/src/main/java/com/example/secondprac/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.secondprac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView out;
+    EditText inp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        out = findViewById(R.id.out);
+        inp = findViewById(R.id.inp);
+    }
+    public void btn(View v){
+//        String str = inp.getText().toString();
+//        out.setText("hi~"+str);
+
+
+    }
+}
